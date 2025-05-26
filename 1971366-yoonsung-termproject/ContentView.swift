368d58be6142fdcdd456933d5092e07fb72a21ef
YOONSUNG <cjfgks9203@naver.com>
2025-05-26 16:14:54 +0900

    Initial Commit

---
+++ 1971366-yoonsung-termproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1971366-yoonsung-termproject
+//
+//  Created by test on 5/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 1971366-yoonsung-termproject/_971366_yoonsung_termprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _971366_yoonsung_termprojectApp.swift
+//  1971366-yoonsung-termproject
+//
+//  Created by test on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct _971366_yoonsung_termprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
