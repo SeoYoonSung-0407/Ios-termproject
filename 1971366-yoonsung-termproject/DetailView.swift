27c76512d4727867b4dae9516219ad0715b53d23
YOONSUNG <cjfgks9203@naver.com>
2025-05-26 19:37:38 +0900

    adding SubViews

---
+++ 1971366-yoonsung-termproject/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  1971366-yoonsung-termproject
+//
+//  Created by test on 5/26/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ 1971366-yoonsung-termproject/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  1971366-yoonsung-termproject
+//
+//  Created by test on 5/26/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
